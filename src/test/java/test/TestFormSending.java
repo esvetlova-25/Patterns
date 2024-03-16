d74f1424b9f7c42d2e04a9c325458042e197a215
Kate test <d.swetlov2017@gmail.com>
2024-03-16 17:00:08 +0300

    modified

--- src/test/java/test/TestFormSending.java
+++ src/test/java/test/TestFormSending.java
@@ -34,9 +34,9 @@ public class TestFormSending {
         $("[data-test-id=phone] input").setValue(validUser.getPhone());
         $("[data-test-id=agreement]").click();
         $(byText("Запланировать")).click();
-        $(byText("Успешно")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
         $("[data-test-id=success-notification].notification__content").shouldBe(Condition.visible);
-        $("[data-test-id=success-notification].notification__content").shouldHave(Condition.exactText("Встреча успешно забронирована на " + firstMeetingDate));
+        $("[data-test-id=success-notification].notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " + firstMeetingDate));
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id=date] input").setValue(secondMeetingDate);
         $(byText("Запланировать")).click();
@@ -44,6 +44,6 @@ public class TestFormSending {
         $("[data-test-id=replan-notification].notification__content").shouldHave(Condition.exactText("У вас уже запланирована встреча на другую дату. Перепланировать?"));
         $(byText("[data-test-id=replan-notification] button")).click();
         $("[data-test-id=success-notification].notification__content").shouldBe(Condition.visible);
-        $("[data-test-id=success-notification].notification__content").shouldHave(Condition.exactText("Встреча успешно забронирована на " + secondMeetingDate));
+        $("[data-test-id=success-notification].notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " + secondMeetingDate));
     }
 }
